98eeba62e39a5681b95a27c732dcd6e97bcb422e
Bernier-Bourdages Yves-Luc <1961716@csfoy.ca>
2022-03-03 13:53:45 -0500

    commit jeudi 3 mars

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { Fragment } from 'react/cjs/react.production.min';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from "./components/Cart/Cart"
 import { useState } from 'react';
+import CartProvider from './store/CartProvider';
 
 function App() {
     const [cartShown, setCartShown] = useState(false);
@@ -17,13 +17,13 @@ function App() {
 
 
     return (
-        <Fragment>
+        <CartProvider>
             {cartShown && <Cart onHideCart={hideCartHandler} />}
             <Header onShowCart={showCartHandler} />
             <main>
                 <Meals />
             </main>
-        </Fragment>
+        </CartProvider>
     );
 }
 
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import { useContext, useEffect, useState } from 'react';
+import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcons';
 
 const HeaderCartButton = (props) => {
+    const [btnJump, setBtnJump] = useState(false);
+    const cartCtx = useContext(CartContext);
+
+    const { items } = cartCtx;
+
+    const numberOfCartItems = items.reduce((previousValue, currrentValue) => {
+        return previousValue + currrentValue.amount;
+    }, 0);
+
+    const btnClasses = `${classes.button} ${btnJump ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+        setBtnJump(true);
+
+        const timer = setTimeout(() => {
+            setBtnJump(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [items]);
+
     return (
-        <button className={classes.button} onClick={props.onShowCart}>
+        <button className={btnClasses} onClick={props.onShowCart}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
             <span>Your Cart</span>
-            <span className={classes.badge}>3</span>
+            <span className={classes.badge}>{numberOfCartItems}</span>
         </button>
     );
 };
